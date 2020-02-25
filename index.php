3a21383a69fe2b27c2a51e85fb13a38b01426bd5
Andre Saddler <dresadd09691@gmail.com>
2020-02-25 16:46:31 -0500

    index.php: Improve description

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php 
 
-echo 'Hello, this is Twitchbot R6S stats/rank script using R6Tab.com API';
+echo 'Hello, this is a R6S stats/rank script for Twitchbot (Nightbot, etc..)  using R6Tab.com API developed by made by <a href="https://github.com/Ayysir" target="_self">Andre Saddler</a>';
+echo 'How-To: http://r6srank.herokuapp.com/r6stats.php?platform=YourPlatformHere&nick=YourNickHere&command=rank';
