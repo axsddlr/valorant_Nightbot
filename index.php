61366dcceda956ae01a1a0817cac567db678281f
Andre Saddler <dresadd09691@gmail.com>
2020-08-03 21:44:07 -0400

    index: fix description

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-echo 'Hello, this is a R6S stats/rank script for Twitchbot (Nightbot, etc..) developed by made by <a href="https://github.com/Rehkloos" target="_self">Andre Saddler</a> using <a href="R6Tab.com" target="_self">R6Tab</a> API '."<br>";
-echo 'My Socials: <a href="https://streamerlinks.com/rehkloos" target="_self">CLICK HERE</a> '."<br><br>";
+echo 'Hello, this is a Valorant API for Twitchbot (Nightbot, etc..) developed by made by <a href="https://github.com/Rehkloos" target="_self">Andre Saddler</a> using <a href="TRN" target="_self">TRN</a> API '."<br>";
+echo 'My Socials: <a href="https://rehkloos.com" target="_self">CLICK HERE</a> '."<br><br>";
 
-echo 'How-To: $(urlfetch http://r6srank.herokuapp.com/r6stats.php?nick=YourNickHere)';
+echo 'How-To: $(urlfetch https://val7.herokuapp.com/val.php?nick=(Name)&id=(ID number after #)&command=stats)';
